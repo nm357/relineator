882ba9a8d542dbd3c8fde85f7f192da2f347ec50
nm357 <n.mikita3@gmail.com>
2019-03-10 03:25:24 -0400

    test Word

--- src/components/Word/Word.test.jsx
+++ src/components/Word/Word.test.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 
+import Word from './Word';
+
 import { shallow } from 'enzyme';
+
+it('renders', () => {
+    shallow(<Word />);
+});
+
+it('displays props.value in a div', () => {
+    const valueProp = 'Relineator'
+    const wrapper = shallow(<Word value={valueProp}/>);
+
+    expect(wrapper.find('div').text()).toEqual(valueProp);
+});
